01628d58e06eaa8b31c88ab5bd5007edd39a5728
Varun Sudrik <varunsudrik99@gmail.com>
2017-05-13 22:17:52 +0530

    Update Solution.cpp

--- 7 - Arrays/Solution.cpp
+++ 7 - Arrays/Solution.cpp
@@ -1,23 +1,38 @@
-#include <algorithm>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
 
-    int arr[N];
-
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
     }
-
-    reverse(arr, arr + N);
-
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
+    for(int arr_i = n-1;arr_i >= 0;arr_i--){
+       cout << arr[arr_i]<<" ";
     }
-
     return 0;
 }
